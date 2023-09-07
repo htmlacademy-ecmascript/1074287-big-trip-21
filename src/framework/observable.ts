41e1d56db22b89beec5bc9650794ac19b65ed50c
Grinder21 <spartawinner280@mail.ru>
2023-09-07 15:08:14 +0300

    convert js to ts framework files

---
+++ src/framework/observable.ts
@@ -0,0 +1,17 @@
+type ObserverCallback = (event: unknown, payload?: unknown) => void;
+
+export default class Observable {
+	#observers = new Set<ObserverCallback>();
+
+	addObserver(observer: ObserverCallback) {
+		this.#observers.add(observer);
+	}
+
+	removeObserver(observer: ObserverCallback) {
+		this.#observers.delete(observer);
+	}
+
+	_notify(event: unknown, payload?: unknown) {
+		this.#observers.forEach((observer) => observer(event, payload));
+	}
+}
